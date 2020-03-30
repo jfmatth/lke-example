7041592b97ff73ba5777972c47139ffe4ba15fcb
John Matthew <john@compunique.com>
2020-03-30 07:26:12 -0700

    minor changes

--- buildsite.sh
+++ buildsite.sh
@@ -1,3 +1,3 @@
-docker build . -t jfmatth/hugoapp
-docker push jfmatth/hugoapp
+docker build . -t jfmatth/lke-example-hugo
+docker push jfmatth/lke-example-hugo
 #helm upgrade hugo lke-chart --set timestamp="$(date)"
